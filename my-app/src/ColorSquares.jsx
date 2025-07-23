0ba3b90afd234bb662958d786e55a838b3c35c8b
Bernard Krehula <krehula.bernard456@gmail.com>
2025-07-23 22:18:49 +0200

    Display squares by difficulty

--- my-app/src/ColorSquares.jsx
+++ my-app/src/ColorSquares.jsx
@@ -4,18 +4,36 @@ import './ColorSquares.css'
 const ColorSquares = ({variation}) => {
     const refColor = useRef(null)
 
+    const generateRandomColor = (difficulty) => {
+        switch(difficulty) {
+            case 'Easy':
+                refColor.current = Array.from({ length: 9 }).map((el, i) => {
+                    const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+                    return color;
+                })
+                break;
+            case 'Medium':
+
+                break;
+            case 'Hard':
+
+                break;
+
+        }
+
+
+        const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        console.log(refColor)
+    }
 
     return(
-        <ul className={`colors ${variation}`}>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+        <ul className={`colors ${variation}`} onClick={() => generateRandomColor(variation)}>
+            {generateRandomColor(variation)}
+            {refColor.current.map((color, index) => {
+                return(
+                    <li key={index} style={{backgroundColor: color}}></li>
+                )
+            })}
         </ul>
     )
 }
