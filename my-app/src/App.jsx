ddc07ef2a1c58e4b9c05411abd4f158c433f4403
Bernard Krehula <krehula.bernard456@gmail.com>
2025-07-27 12:52:53 +0200

    change difficulty move to app.jsx file

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,6 +5,31 @@ import ColorSquares from './ColorSquares';
 
 function App() {
   const [ difficulty, setDifficulty ] = useState('Easy');
+  const [ colors, setColors ] = useState([]);
+  
+   const changeDifficulty = (variation) => {
+        const randomColor = (count) => {
+            const newColors = Array.from({ length: count }).map((_, index) => ({
+                id: crypto.randomUUID(),
+                color: "#" + Math.floor(Math.random() * 16777215).toString(16),
+                isClicked: false
+            }));
+            setColors(newColors)
+        }
+     
+        switch(variation) {
+            case 'Easy':
+                randomColor(9)
+                break;
+            case 'Medium':
+                randomColor(16)
+                break;
+            case 'Hard':
+                randomColor(25)
+                break;
+            }
+    }
+    if(colors.length === 0) changeDifficulty(difficulty)
 
   return (
     <>
@@ -13,11 +38,11 @@ function App() {
         <p className='subheadline'>The goal of the game is to click on all squares only once! If you click on the same square twice, you loose!</p>
         <div className='game-content'>
           <ul className='difficulty-level'>
-            <Button onClick={() => {setDifficulty('Easy')}}>Easy</Button>
-            <Button onClick={() => {setDifficulty('Medium')}}>Medium</Button>
-            <Button onClick={() => {setDifficulty('Hard')}}>Hard</Button>
+            <Button onClick={() => {setDifficulty('Easy'); changeDifficulty('Easy');}}>Easy</Button>
+            <Button onClick={() => {setDifficulty('Medium'); changeDifficulty('Medium');}}>Medium</Button>
+            <Button onClick={() => {setDifficulty('Hard'); changeDifficulty('Hard');}}>Hard</Button>
           </ul>
-          <ColorSquares variation={difficulty} />
+          <ColorSquares variation={difficulty} colors={colors} setColors={setColors}/>
         </div>
       </div>
     </>
--- my-app/src/ColorSquares.jsx
+++ my-app/src/ColorSquares.jsx
@@ -3,8 +3,7 @@ import './ColorSquares.css'
 import SingleList from './SingleList';
 
 
-const ColorSquares = ({variation}) => {
-    const [ colors, setColors ] = useState([]);
+const ColorSquares = ({variation, colors, setColors}) => {
     const [ disabled, setDisabled ] = useState(false);
     
     const setIsClicked = (id) => {
@@ -13,40 +12,16 @@ const ColorSquares = ({variation}) => {
     const changeSquarePosition = () => {
         setColors(prev => [...prev].sort(() => Math.random() - 0.5));
     }
-    //Staviti const koja racuna koliko je colors sa isClicked true
     const sumClicks = colors.filter(color => color.isClicked === true).length;
   
     const setIsClickedToFalse = () => {
         setColors(prev => prev.map(square => ({...square, isClicked: false})))
     }
     //Funckija change difficulty pozivam na klik
-    const changeDifficulty = () => {
-        const randomColor = (count) => {
-            const newColors = Array.from({ length: count }).map((_, index) => ({
-                id: crypto.randomUUID(),
-                color: "#" + Math.floor(Math.random() * 16777215).toString(16),
-                isClicked: false
-            }));
-            setColors(newColors)
-        }
-     
-        switch(variation) {
-            case 'Easy':
-                randomColor(9)
-                break;
-            case 'Medium':
-                randomColor(16)
-                break;
-            case 'Hard':
-                randomColor(25)
-                break;
-            }
-    }
-    
+   
     return(
         <>
             <ul className={`colors ${variation}`} onClick={changeSquarePosition} style={{pointerEvents: disabled ? 'none' : 'auto'}}>
-                {variation && colors.length === 0 ? changeDifficulty() : ''}
                 {colors.map((colorsInfo) => {
                     const { id, color, isClicked } = colorsInfo;
                 
